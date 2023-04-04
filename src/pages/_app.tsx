2518e1258a1d9b8c6edf7d9285ddad1170f22503
Piero Rolando <rolandopiero63@gmail.com>
2023-04-04 18:51:33 -0500

    Add navbar component to the app main container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className='max-w-4xl mx-auto flex'>
+      <Navbar />
+      <Component {...pageProps} />
+    </main>
+  )
 }
