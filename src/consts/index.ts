83af7ab3f95ef62acd60954c43b20f226e0075e5
Piero Rolando <rolandopiero63@gmail.com>
2023-04-18 18:41:28 -0500

    Add consts for Alfred image and welcome message to be used as a personal assistant introduction

---
+++ src/consts/index.ts
@@ -0,0 +1,2 @@
+export const ALFRED_IMAGE = "https://www.lego.com/cdn/cs/set/assets/blteaccd97d04f9673d/DC_-_Character_-_Details_-_Sidekick-Standard_-_Alfred.jpg"
+export const WELCOME_MESSAGE = "Hello there! I'm Alfred, the personal assistant and representative of Piero Rolando, a self-taught programmer who's currently studying Computer Science. I'm the one who makes sure Piero doesn't forget to eat or sleep while he's busy coding his way to world domination. (You can ask me things about him in ANY LANGUAGE) "
