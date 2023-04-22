03c31f09debcb2e0ceb1f70a031f1e6a90a61e6a
Piero Rolando <rolandopiero63@gmail.com>
2023-04-22 10:39:41 -0500

    Add OPENAI_API_KEY to env and update dependencies in package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.experiments = { asyncWebAssembly: true, layers: true  };
     return config;
