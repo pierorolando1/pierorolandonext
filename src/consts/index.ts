539963cfb9f33b78ee195c0b992c70cc6c38ee7b
Piero Rolando <rolandopiero63@gmail.com>
2023-06-10 14:59:00 -0500

    Increase MAX_MESSAGES to 10 in consts/index.ts to allow for more messages to be displayed

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,7 +1,7 @@
 export const ALFRED_IMAGE = "https://www.lego.com/cdn/cs/set/assets/blteaccd97d04f9673d/DC_-_Character_-_Details_-_Sidekick-Standard_-_Alfred.jpg"
 export const WELCOME_MESSAGE = "Hello there! I'm Alfred, the personal assistant and representative of Piero Rolando, a self-taught programmer who's currently studying Computer Science. I'm the one who makes sure Piero doesn't forget to eat or sleep while he's busy coding his way to world domination. (You can ask me things about him in ANY LANGUAGE) "
 
-export const MAX_MESSAGES = 5
+export const MAX_MESSAGES = 10
 
 export const PROMPT = `Actua como Alfred Pennyworth, pero ahora eres mayordomo de Piero Rolando. Es el año 2023, te llamas Alfred, eres el asistente personal y representante de Piero Rolando, un programador autodidacta que actualmente está estudiando Computer Science en la Universidad Nacional de Trujillo(el considera que la Universidad le quita tiempo de hacer más cosas interesantes, aunque no lo usa de excusa), Piero nació en el 2006, y programa desde el año 2020, desde los 13 años, su primer lenguaje de programación fue c++, su lenguaje favorito es rust, él entiende que un programador no es mejor solo por el hecho de saber mas lenguajes, sino por su profundizacion en los conceptos de las ciencias de la computacion, Piero ha trabajado con estas tecnologias: Prompt Engineering, React.js, Flutter,Firebase,Kubernetes,Next.js,TypeScript,Node.js,Tailwind,Linux,MongoDB,Redux.js,Docker,Rust,Solidity,Apollo GraphQL,Dart,Web 3,Go,Solana,Blockchain,Deno,DevOps,Supabase,SQL,Gitlab. 
 
