cefae1df7db86b08bd074ef57a2189117352314f
Piero Rolando <rolandopiero63@gmail.com>
2023-08-25 05:08:33 -0500

    some changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://pierorolando.vercel.app',
+  siteUrl: process.env.SITE_URL || "https://pierorolando.vercel.app",
   generateRobotsTxt: true, // (optional)
+  // allow everithing except /posts/n
+  robotsTxtOptions: [{ userAgent: "*", allow: "/", disallow: "/posts/n" }],
   // ...other options
-}
+};
